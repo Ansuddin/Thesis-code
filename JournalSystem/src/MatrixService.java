f4936a35c75d7234f46471e0bebbeee812e0fa48
Ans Uddin <ansuddin7@gmail.com>
2019-01-07 11:21:17 +0100

    minor changes

---
+++ JournalSystem/src/MatrixService.java
@@ -0,0 +1,42 @@
+import org.jpy.PyLib;
+import org.jpy.PyModule;
+import org.jpy.PyObject;
+import org.jpy.PyInputMode;
+
+public class MatrixService implements Matrix {
+	public void send(String message) {
+
+	}
+
+	public String retrive() {
+		return null;
+	}
+	public void join(){
+
+	}
+
+	public void register(){
+
+	}
+
+	public void login() {
+
+	}
+
+	public void invite() {
+
+	}
+
+	public void ban() {
+
+	}
+
+	public void unban() {
+
+	}
+
+	public String[] getParticipants() {
+		return null;
+	}
+
+}
