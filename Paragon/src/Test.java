56fc73f9251a0d134d912e2eb7bdeec96b4e294c
Ans Uddin <ansuddin7@gmail.com>
2019-01-11 12:46:39 +0100

    program with liberal policies and no error handling

--- Paragon/src/Test.java
+++ Paragon/src/Test.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import journalsystem.*;
 import java.util.Scanner;
 import java.lang.System;
+import java.util.ArrayList;
 public class Test
 {
   public static void main(java.lang.String[] args)
@@ -17,6 +18,7 @@ public class Test
       {
         try
         {
+          journalsystem.MatrixApi api = new journalsystem.MatrixApi("https://ansuddin.xyz", "ans", "test", "!XtUmMyjaNdBdSbMfxK:ansuddin.xyz");
           Employee employee;
           if (args[0].equals("doctor"))
           {
@@ -38,22 +40,96 @@ public class Test
                          java.util.Scanner scanner = new java.util.Scanner(java.lang.System.in);
                          java.lang.System.out.println("Select an option: ");
                          java.lang.System.out.println("1. Retrieve patient journal");
-                         java.lang.System.out.println("2. Edit and send patient journal");
-                         java.lang.System.out.println("3. Exit");
+                         java.lang.System.out.println("2. Add and send new patient journal");
+                         java.lang.System.out.println("3. Edit and send patient journal");
+                         java.lang.System.out.println("4. Exit");
                          int option = scanner.nextInt();
                          if (option == 1)
                          {
-                           java.lang.System.out.println("Retrieved journal");
-                           break;
+                           java.lang.System.out.println("Retrieving journal");
+                           java.lang.String json = api.retrieve();
+                           if (json == null || json.equals(""))
+                           {
+                             java.lang.System.out.println("Journal not added");
+                           }
+                           else
+                           {
+                             java.lang.System.out.println(json);
+                           }
+                           java.lang.System.out.println();
                          }
                          else
                          if (option == 2)
                          {
-                           java.lang.System.out.println("Editing and sending journal");
-                           break;
+                           java.lang.System.out.println("Adding and sending new journal");
+                           java.lang.String json = api.retrieve();
+                           if (json == null || json.equals(""))
+                           {
+                             java.lang.String[] sessions = {"started treatment for pollen"};
+                             PatientJournal journal = new PatientJournal(1, "270793-2131", "Ans Uddin", "Knapmagerstien 49", "Has grass pollen allergy", sessions);
+                             json = api.journalToString(journal);
+                             api.send(json);
+                             java.lang.System.out.println(json);
+                             java.lang.System.out.println("Journal added");
+                           }
+                           else
+                           {
+                             java.lang.System.out.println("Journal already exists");
+                           }
+                           java.lang.System.out.println();
                          }
                          else
                          if (option == 3)
+                         {
+                           java.lang.System.out.println("Editing and sending journal");
+                           java.lang.String json = api.retrieve();
+                           if (json != null)
+                           {
+                             if (!json.equals(""))
+                             {
+                               PatientJournal journal = api.stringToJournal(json);
+                               java.lang.System.out.println("Select option:");
+                               java.lang.System.out.println("1. Change public note");
+                               java.lang.System.out.println("2. Add session");
+                               int editOption = scanner.nextInt();
+                               java.util.Scanner editScanner = new java.util.Scanner(java.lang.System.in);
+                               if (editOption == 1)
+                               {
+                                 java.lang.System.out.println("Add new public note");
+                                 java.lang.String note = editScanner.nextLine();
+                                 journal.publicNotes = note;
+                                 json = api.journalToString(journal);
+                                 api.send(json);
+                               }
+                               else
+                               if (editOption == 2)
+                               {
+                                 java.lang.System.out.println("Add new session");
+                                 java.lang.String newSession = editScanner.nextLine();
+                                 journal.addToSessions(newSession);
+                                 json = api.journalToString(journal);
+                                 api.send(json);
+                               }
+                               else
+                               {
+                                 java.lang.System.out.println("Wrong option!");
+                               }
+                               java.lang.System.out.println(json);
+                               java.lang.System.out.println("Journal edited");
+                             }
+                             else
+                             {
+                               java.lang.System.out.println("Journal not added.");
+                             }
+                           }
+                           else
+                           {
+                             java.lang.System.out.println("Journal not added");
+                           }
+                           java.lang.System.out.println();
+                         }
+                         else
+                         if (option == 4)
                          {
                            java.lang.System.out.println("Exiting");
                            break;
--- Paragon/src/journalsystem/PatientJournal.java
+++ Paragon/src/journalsystem/PatientJournal.java
@@ -1,18 +1,33 @@
 package journalsystem;
+import java.util.ArrayList;
 public class PatientJournal
 {
   public int id;
   public java.lang.String ssn;
   public java.lang.String name;
   public java.lang.String address;
+  public java.lang.String publicNotes;
+  public java.lang.String[] sessions;
   public PatientJournal ()
   {
   }
-  public PatientJournal (int id, java.lang.String ssn, java.lang.String name, java.lang.String address)
+  public PatientJournal (int id, java.lang.String ssn, java.lang.String name, java.lang.String address, java.lang.String publicNotes, java.lang.String[] sessions)
   {
     this.id = id;
     this.ssn = ssn;
     this.name = name;
     this.address = address;
+    this.publicNotes = publicNotes;
+    this.sessions = sessions;
+  }
+  public void addToSessions(java.lang.String session)
+  {
+    java.lang.String[] newSessions = new java.lang.String[this.sessions.length + 1];
+    for (int i = 0 ; i < this.sessions.length ; i++)
+    {
+      newSessions[i] = this.sessions[i];
+    }
+    newSessions[sessions.length] = session;
+    this.sessions = newSessions;
   }
 }
