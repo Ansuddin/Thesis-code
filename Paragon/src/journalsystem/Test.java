210c4350d6f6942344abccded6492a398e3674b6
Ans Uddin <ansuddin7@gmail.com>
2019-02-24 14:32:52 +0100

    some ideas for code

--- Paragon/src/journalsystem/Test.java
+++ Paragon/src/journalsystem/Test.java
@@ -8,7 +8,9 @@ public class Test
 {
   public static void main(java.lang.String[] args)
   {
-    java.lang.String name = "Ans";
-    java.lang.String test = name;
+    Secretary untrustedObserver = new Secretary();
+    Doctor trustedObserver = new Doctor();
+    se.chalmers.paragon.runtime.Policy untrusted = new se.chalmers.paragon.runtime.Policy(new se.chalmers.paragon.runtime.Clause(new Class<?>[] {}, new se.chalmers.paragon.runtime.Actor(untrustedObserver)));
+    se.chalmers.paragon.runtime.Policy trusted = new se.chalmers.paragon.runtime.Policy(new se.chalmers.paragon.runtime.Clause(new Class<?>[] {}, new se.chalmers.paragon.runtime.Actor(untrustedObserver)), new se.chalmers.paragon.runtime.Clause(new Class<?>[] {}, new se.chalmers.paragon.runtime.Actor(trustedObserver)));
   }
 }
