3815af5fc1d02ad2bbfee2ea8f6ca1fc6b7de257
Ans Uddin <ansuddin7@gmail.com>
2019-02-25 17:14:25 +0100

    patientjournal implemented with proper policy

--- Paragon/src/journalsystem/Locks.java
+++
@@ -1,6 +0,0 @@
-public class Locks
-{
-  static final public se.chalmers.paragon.runtime.LockID IsDoctor = new se.chalmers.paragon.runtime.LockID("IsDoctor");
-  static final public se.chalmers.paragon.runtime.LockID IsReferred = new se.chalmers.paragon.runtime.LockID("IsReferred", Doctor.class, PatientJournal.class);
-  static final public se.chalmers.paragon.runtime.LockID Referred = new se.chalmers.paragon.runtime.LockID("Referred");
-}
